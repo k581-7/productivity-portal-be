d0cd14435e3fae484370deb275f3bfb62505ed09
Karla Patricia Farinas <karlafarinas@gmail.com>
2025-10-28 01:42:58 +0800

    Fixed code for user controller to allow Leader role users to access the list of junior users in the Assign to User dropdown.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,8 +1,18 @@
 class Api::V1::UsersController < ApplicationController
-  before_action :authorize_dev!, only: [:index, :update_role]
+  before_action :authorize_dev!, only: [:update_role]
+  before_action :authorize_dev_or_leader!, only: [:index]
 
   def index
-    users = User.all.order(:email)
+    # Developers can see all users
+    # Leaders can only see junior users
+    if current_user.developer?
+      users = User.all.order(:email)
+    elsif current_user.leader?
+      users = User.where(role: :junior).order(:email)
+    else
+      users = []
+    end
+    
     render json: users.as_json(only: [:id, :name, :email, :role])
   end
 
@@ -32,6 +42,12 @@ class Api::V1::UsersController < ApplicationController
 
   def authorize_dev!
     unless current_user&.role == 'developer'
+      render json: { error: 'Forbidden' }, status: :forbidden
+    end
+  end
+
+  def authorize_dev_or_leader!
+    unless current_user&.developer? || current_user&.leader?
       render json: { error: 'Forbidden' }, status: :forbidden
     end
   end
